517353d5e817653fc570bed50b543782a6079a1e
Fahad-78 <fahadsorkar23@gmail.com>
2026-08-08 05:55:34 +0600

    Add initial documentation for class relationships, including aggregation and inheritance

---
+++ Python Fundamentals from CampusX/Session9 OOP part3/1Aggregation(Has-A-relationship).py
@@ -0,0 +1,11 @@
+'''
+There are tow types of class relationship
+    ->Aggregation
+    ->Inheritence
+    
+Inheritence is mostly used, but we will also learn aggregation
+'''
+#example
+#method
+#
+
